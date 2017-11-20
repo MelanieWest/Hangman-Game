58c332eafa1b377b812d12dbd36ca396dbcb98a4
Melanie West <dhmofan@gmail.com>
2017-11-20 17:06:14 -0500

    Clean page appearance by putting stats in the header.  Also enable topic buttons by replacing the 'start' on click to 'btn' on click, then selecting button id to identify the topic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,88 +1,99 @@
-var wordNum =0;
+var wordNum = 0;
 var numGuesses = 0;
 var numMisses = 0;
 var Wins = 0;
 var Losses = 0;
-var word='';
-var lettersChosen=[];
-var displaySol=[];
+var word = '';
+var lettersChosen = [];
+var displaySol = [];
 var solutionShow = '';
 
 // Word bank, chosen from 'word of the day' in dictionary app
 // word banks for life and science have been added for future options (not yet used)
 
-var wordBank =['FUNSTER', 'GRAVID','COMSTOCKERY','THEINE','CONFABULATE','ULULATE','ICONOCLASTIC',
-'ARVO','VENTIFACT','PALINDROME', 'POLEMIC', 'KUMQUAT', 'TACO', 'BANAUSIC', 'DAYMARE', 
-'KIBITZER', 'PALUDAL', 'NIMBUS', 'ERGATE', 'MUSSITATION', 'FRUCTIFY','CONSUETUDE','INCOGNITANT',
-'DORP','ANOESIS','VARIEGATED','BALLON','BUMFUZZLE','AUTARKY','LITOTES','HIGHFALUTIN'];
-var wordlife =[ 'FLOWER', 'MANGO', 'URCHIN','EXERCISE','DIET','EAT','REPRODUCE','BREATHE','ANGEL','DEMON', 
-'EGGROLL','LEMON', 'AVATAR','TACO', 'MUSIC','SANDWICH','COMPLEX','LOVE','EPIPHANY','NEMESIS'];
-var wordSci =['QUARK','PULSAR','CUMULUS','PHASE','FUNCTION','SINGULARITY','STATE',
-'GRAVITY','ENERGY','FUNDAMENTAL','LAWS','PROPERTIES','CONDENSE','ESSENCE','WAVES'];
-var wordEasy = ['APPLE','PUPPY','KITTEN','HOUSE','SOCCER','LUNCH','BASEBALL','GAME'];
-
-//topic selector buttons added to vary word banks (still need to make functions)
-
-// document.getElementById("day").addEventListener("click",wordBankselectorfcn);
-// document.getElementById("life").addEventListener("click",wordLifeselectorfcn);
-// document.getElementById("sci").addEventListener("click",wordSciselectorfcn);
-// document.getElementById("easy").addEventListener("click",wordEasyselectorfcn);
+var wordBank = ['FUNSTER', 'GRAVID', 'COMSTOCKERY', 'THEINE', 'CONFABULATE', 'ULULATE', 'ICONOCLASTIC',
+  'ARVO', 'VENTIFACT', 'PALINDROME', 'POLEMIC', 'KUMQUAT', 'TACO', 'BANAUSIC', 'DAYMARE',
+  'KIBITZER', 'PALUDAL', 'NIMBUS', 'ERGATE', 'MUSSITATION', 'FRUCTIFY', 'CONSUETUDE', 'INCOGNITANT',
+  'DORP', 'ANOESIS', 'VARIEGATED', 'BALLON', 'BUMFUZZLE', 'AUTARKY', 'LITOTES', 'HIGHFALUTIN'];
+var wordLife = ['FLOWER', 'MANGO', 'URCHIN', 'EXERCISE', 'DIET', 'EAT', 'REPRODUCE', 'BREATHE', 'ANGEL', 'DEMON',
+  'EGGROLL', 'LEMON', 'AVATAR', 'TACO', 'MUSIC', 'SANDWICH', 'COMPLEX', 'LOVE', 'EPIPHANY', 'NEMESIS'];
+var wordSci = ['QUARK', 'PULSAR', 'CUMULUS', 'PHASE', 'FUNCTION', 'SINGULARITY', 'STATE',
+  'GRAVITY', 'ENERGY', 'FUNDAMENTAL', 'LAWS', 'PROPERTIES', 'CONDENSE', 'ESSENCE', 'WAVES'];
+var wordEasy = ['APPLE', 'PUPPY', 'KITTEN', 'HOUSE', 'SOCCER', 'LUNCH', 'BASEBALL', 'GAME'];
 
 
-//A 'start' button has been added - it is used to reset the word and guess count
+//Topic buttons start the game and select the word bank; they reset the word and guess count
 
-$("#start").on("click", function(){ // initialize word and counts with 'start' button
-  word = ranWord(wordBank);     //select a new word
+$(".btn").on("click", function () { // initialize word and stats with a topic button
+
+  switch (this.id) {    //choose a random word from the selected word bank
+    case "day":
+      word = ranWord(wordBank);
+      break;
+    case "life":
+      word = ranWord(wordLife);
+      break;
+    case "sci":
+      word = ranWord(wordSci);
+      break;
+    case "easy":
+      word = ranWord(wordEasy);
+      break;
+  }
+
+
   lettersChosen = [];   //zero out string of guesses
   numMisses = 0;
-   //initialize the solution display (all blanks)
-  displaySol= initBlanks(word);     //set up an array of the correct # of blanks
+  //initialize the solution display (all blanks)
+  displaySol = initBlanks(word);     //set up an array of the correct # of blanks
   //make text display better (without commas)
   solutionShow = printToDocument(displaySol);
-  lettersShow  = printToDocument(lettersChosen);
+  lettersShow = printToDocument(lettersChosen);
   //keep stats current
-  Stats(10-numMisses, lettersShow, solutionShow ,Wins,Losses);
+  Stats(10 - numMisses, lettersShow, solutionShow, Wins, Losses);
   document.getElementById("message1").innerHTML = ' ';
+
+
 });     //end of 'start' button block
 
 
 
 
 // this next block executes until the word is completed or guesses are used up
-    
-document.onkeyup = function(event) {        //this is where iterations begin --word and blanks are set already
+
+document.onkeyup = function (event) {        //this is where iterations begin --word and blanks are set already
 
   // Determines which letter was selected.
   var userLet = event.key;
   console.log(userLet);
-  
+
   //store each guess into a new string for on-screen display of all guesses
 
   userLet = userLet.toUpperCase();    //all uppercase letters
   lettersChosen.push(userLet)   //append array for display
-    
+
   match = false;   // set to false until proven otherwise
 
   // reset the displayed partial word to include correctly guessed letters.  If none, count as a miss
 
-  for (var j=0; j < word.length; j++){      
-    if(word[j]==userLet) {
+  for (var j = 0; j < word.length; j++) {
+    if (word[j] == userLet) {
       displaySol[j] = userLet;
       match = true;
     }
   }
 
 
-  numGuesses +=1;   // increment before stats are displayed
-  
-  
+  numGuesses += 1;   // increment before stats are displayed
+
+
   // if the entire word is solved before guesses have been used up, increment
   // 'wins' by 1 and set up a new word. (or prompt for another game)
-  if (isSolved(word, displaySol) == true){
+  if (isSolved(word, displaySol) == true) {
     document.getElementById("message1").innerHTML = 'Congratulations!  You win!';
     numMisses = 0;
     numGuesses = 0;
-    lettersChosen = []; 
+    lettersChosen = [];
     Wins += 1;
   }
   // if all guesses have been used up before the word is solved, show answer, 
@@ -90,71 +101,71 @@ document.onkeyup = function(event) {        //this is where iterations begin --w
   //count misses
   if (match == false) {   //if not in word, increment misses up to max value of 10
     numMisses += 1;
-    if(numMisses == 10){
-       document.getElementById("message1").innerHTML = 'No more guesses! Your word was: ' + word;
-       numGuesses = 0;
-       lettersChosen = []; 
-       Losses += 1;  
+    if (numMisses == 10) {
+      document.getElementById("message1").innerHTML = 'No more guesses! Your word was: ' + word;
+      numGuesses = 0;
+      lettersChosen = [];
+      Losses += 1;
     }
   }
-  
- 
+
+
   document.getElementById("incWord").innerHTML = displaySol;
-  
+
   // make answer display better (without commas)
-    solutionShow = printToDocument(displaySol);
-    lettersShow = printToDocument(lettersChosen);
+  solutionShow = printToDocument(displaySol);
+  lettersShow = printToDocument(lettersChosen);
 
   //keep stats current
-  Stats(10-numMisses, lettersShow, solutionShow, Wins, Losses);
+  Stats(10 - numMisses, lettersShow, solutionShow, Wins, Losses);
 
-  
+
 }   // end of new letter function
 
 //-------------------------begin function definitions------------------------------------
-  // function for keeping stats updated
+// function for keeping stats updated
 
-  function Stats( m,letters,inc,w,l){     
-    document.getElementById("misses").innerHTML = m;
-    document.getElementById("letterslist").innerHTML = letters;
-    document.getElementById("incWord").innerHTML = inc;
-    document.getElementById("wins").innerHTML = w;
-    document.getElementById("losses").innerHTML = l;
-  }
+function Stats(m, letters, inc, w, l) {
+  document.getElementById("misses").innerHTML = m;
+  document.getElementById("letterslist").innerHTML = letters;
+  document.getElementById("incWord").innerHTML = inc;
+  document.getElementById("wins").innerHTML = w;
+  document.getElementById("losses").innerHTML = l;
+}
 
-  function ranWord(wordArray){
-    randomWord = wordArray[Math.floor(Math.random()*wordArray.length)] ;   //new randomly selected word
-    console.log(randomWord);
+function ranWord(wordArray) {
+  randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];   //new randomly selected word
+  console.log(randomWord);
   return randomWord;
- }
+}
 
 
-  //function for updating display after each new word is selected
-  function initBlanks(answer){
-    var blanks = [];
-    for (var index= 0; index < answer.length; index++){
-        blanks.push('_');
+//function for updating display after each new word is selected
+function initBlanks(answer) {
+  var blanks = [];
+  for (var index = 0; index < answer.length; index++) {
+    blanks.push('_');
+  }
+  return blanks;
+}
+
+function printToDocument(d) {
+  printDoctoString = d.join(' ');
+  // var printDoc = '';
+  // for(var i = 0; i< d.length; i++) {
+  //   printDoc += d[i]+' ';
+  // }
+  // return printDoc;
+  return printDoctoString;
+}
+
+//function for checking if word is completely solved
+function isSolved(w, d) {
+  complete = true;
+  for (var k = 0; k < w.length; k++) {
+    if (w[k] != d[k]) {   //compare their solution to actual
+      complete = false;
     }
-    return blanks;
   }
-
-  function printToDocument(d){
-    printDoctoString = d.join(' ');
-    // var printDoc = '';
-    // for(var i = 0; i< d.length; i++) {
-    //   printDoc += d[i]+' ';
-    // }
-    // return printDoc;
-    return printDoctoString;
-  }
-
-  //function for checking if word is completely solved
-   function isSolved(w,d){
-     complete = true;
-     for(var k =0; k < w.length; k++){
-        if (w[k] != d[k]) {   //compare their solution to actual
-          complete = false;
-        }
-     }
-     return complete;
-   }
+  return complete;
+}
